5837056977da0c6c0895a4f5b44f68050f2e76f6
Mateusz Ossolinski <mateusz.ossolinski@weareams.com>
2024-06-22 14:17:22 +0200

    --amend

--- examples/module1/lesson1/solver/App.tsx
+++ examples/module1/lesson1/solver/App.tsx
@@ -1,52 +1,27 @@
-import React, { memo, useState } from 'react';
-import { sum, subtract, multiply, divide } from './functions';
-import { late } from 'zod';
-
-  const ActionButton = memo(({ label, onClick }: { label: string, onClick: () => void }) => {
-    return (
-       <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={onClick}
-        >
-          {label}
-        </button>
-    )
-  })
-
+import { sum, subtract, multiply, divide } from './calculator/functions';
+import { CalculatorButton } from './calculator/CalculatorButton';
+import { useCalculator } from './calculator/useCalculator';
 
 const App = () => {
-  const [firstValue, setFirstValue] = useState<number>(0);
-  const [secondValue, setSecondValue] = useState<number>(0);
-  const [result, setResult] = useState<number>(0);
-  const [error, setError] = useState<string>('')
+  const { firstInputValue, setFirstInputValue, secondInputValue, setSecondInputValue, handleOperation, error, result } = useCalculator()
 
-    const handleOperation = (operation: (a: number, b: number) => number) => {
-    if (operation === divide && secondValue === 0) {
-      setError('Error: Division by zero');
-      setResult(0);
-    } else {
-      setResult(operation(firstValue, secondValue));
-      setError('');
-    }
-  }
-
-  const actions =  [{
+  const calculatorOperators =  [{
     id: 1,
-          label: '+',
-          onClick: () => handleOperation(sum)
-        }, {
-          id: 2,
-          label: '-',
-          onClick: () => handleOperation(subtract)
-        }, {
-          id: 3,
-          label: '*',
-          onClick: () => handleOperation(multiply)
-        }, {
-          id: 4,
-          label: '/',
-          onClick: () => handleOperation(divide)
-        }]
+    label: '+',
+    onClick: () => handleOperation(sum)
+  }, {
+    id: 2,
+    label: '-',
+    onClick: () => handleOperation(subtract)
+  }, {
+    id: 3,
+    label: '*',
+    onClick: () => handleOperation(multiply)
+  }, {
+    id: 4,
+    label: '/',
+    onClick: () => handleOperation(divide)
+  }]
 
   return (
     <div>
@@ -54,19 +29,19 @@ const App = () => {
         <input
           type="number"
           className="rounded-md shadow-md p-4"
-          value={firstValue}
-          onChange={(e) => setFirstValue(parseFloat(e.target.value))}
+          value={firstInputValue}
+          onChange={(e) => setFirstInputValue(parseFloat(e.target.value))}
         />
         <input
           type="number"
           className="rounded-md shadow-md p-4"
-          value={secondValue}
-          onChange={(e) => setSecondValue(parseFloat(e.target.value))}
+          value={secondInputValue}
+          onChange={(e) => setSecondInputValue(parseFloat(e.target.value))}
         />
       </div>
       <div className="grid grid-cols-4 gap-x-4 my-4">
-        {actions.map(({ id, label, onClick }) => (
-          <ActionButton
+        {calculatorOperators.map(({ id, label, onClick }) => (
+          <CalculatorButton
             key={id}
             label={label}
             onClick={onClick}
