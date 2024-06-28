2aca51ae63e10563bc8be77aba03f3344a52677d
Mateusz Ossolinski <mateusz.ossolinski@weareams.com>
2024-06-28 07:29:16 +0200

    refactor: moved switching app logic to context

--- examples/module1/lesson2/rest-countries-api/App.tsx
+++ examples/module1/lesson2/rest-countries-api/App.tsx
@@ -1,33 +1,11 @@
 import "./App.css";
-
-import { useState } from "react";
-import { CountriesSearch } from "./containers/CountriesSearch";
-import { CountriesGuesser } from "./containers/CountriesGuesser";
-
-enum AppType {
-  COUNTRY_SEARCH = 'COUNTRY_SEARCH',
-  COUNTRY_GUESSER = 'COUNTRY_GUESSER',
-}
-
-const appTypeComponents = {
-  [AppType.COUNTRY_SEARCH]: <CountriesSearch />,
-  [AppType.COUNTRY_GUESSER]: <CountriesGuesser />
-};
+import { AppSwitcherProvider } from "./context/useAppSwitcher";
 
 function App() {
-  const [appType, setAppType] = useState(AppType.COUNTRY_SEARCH);
-
   return (
-    <div className="p-4">
-      <div className="flex gap-4">
-        <button onClick={() => setAppType(AppType.COUNTRY_SEARCH)}>COUNTRY SEARCH</button>
-        <button onClick={() => setAppType(AppType.COUNTRY_GUESSER)}>COUNTRY GUESSER</button>
-      </div>
-
-      <div className="mt-5">
-        {appTypeComponents[appType] || <div>Error</div>}
-      </div>
-    </div>
+    <AppSwitcherProvider>
+      <></>
+    </AppSwitcherProvider>
   );
 }
 
