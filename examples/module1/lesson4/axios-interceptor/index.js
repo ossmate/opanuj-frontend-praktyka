e897cb96282d78ca58f41ee58c80f69ff979a251
Mateusz Ossolinski <mateusz.ossolinski@weareams.com>
2024-07-01 19:07:55 +0200

    feat: added error handling for interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -17,6 +17,17 @@ axios.interceptors.response.use(response => {
   );
 
   return response
+}, (error) => {
+    if (error.config && error.config.metadata) {
+    const endTime = new Date();
+    const duration = endTime - error.config.metadata.startTime;
+    error.durationInMs = duration;
+
+    console.log(
+        `${response.config.method.toUpperCase()} request under URL ${response.config.url} failed after: ${response.durationInMs} ms`
+      );
+    }
+    return Promise.reject(error);
 });
 
 const {
